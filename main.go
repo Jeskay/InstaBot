d051456f9e8cc8f08b06a6c9ddc477f81eefae7c
Valerii Khrolenko <37577958+Jeskay@users.noreply.github.com>
2023-07-25 22:24:25 +1000

    profile fetching added

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func init() {
 
 var insta *goinsta.Instagram
 
-func login() {
+func login(username string, password string) {
 	if err := reloadSession(); err != nil {
-		createSession()
+		createSession(username, password)
 	}
 }
 
@@ -37,9 +37,8 @@ func reloadSession() error {
 	return nil
 }
 
-func createSession() {
-	conf := config.NewConfig()
-	inst := goinsta.New(conf.Instagram.Username, conf.Instagram.Password)
+func createSession(username string, password string) {
+	inst := goinsta.New(username, password)
 	insta = inst
 	if err := insta.Login(); err != nil {
 		panic(err)
@@ -52,6 +51,13 @@ func createSession() {
 }
 
 func main() {
-	login()
+	conf := config.NewConfig()
+	login(conf.Instagram.Username, conf.Instagram.Password)
+	for _, donor := range conf.Instagram.Donors {
+		if profile, err := insta.VisitProfile(donor); err == nil {
+			user := profile.User
+			log.Printf("%s has %d followers, %d posts, and %d IGTV vids \n", user.Username, user.FollowerCount, user.MediaCount, user.IGTVCount)
+		}
+	}
 	log.Print(insta.Account.FullName)
 }
